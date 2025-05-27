31ed21e0caf9f5095091827d9184563055cca4b4
KaungHtetLwin <phoekaung.3819@gmail.com>
2025-05-27 20:26:16 +0630

    update

---
+++ src/components/Home/HeadBar.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const HeadBar = () => {
+  return (
+    <div className='text-4xl font-black'>HeadBar</div>
+  )
+}
---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { HeadBar } from './HeadBar'
+
+export const Home = () => {
+  return (
+   <>
+    <HeadBar/>
+   </>
+  )
+}
